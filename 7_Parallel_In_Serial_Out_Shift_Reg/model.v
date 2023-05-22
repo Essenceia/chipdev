f5bfa4a882d010148ed005dc87155ead9e622ca3
Julia Desmazes <julia.desmazes@gmail.com>
2023-05-22 10:39:47 -0700

    RTL : Added formal for reg shift

--- 7_Parallel_In_Serial_Out_Shift_Reg/model.v
+++ 7_Parallel_In_Serial_Out_Shift_Reg/model.v
@@ -27,6 +27,25 @@ assign dout = data_q[0];
 
 `ifdef FORMAL
 
+reg  v_f_q;
+wire v_f_next;
+reg  [DATA_WIDTH-1:0] din_f_q;
+wire [DATA_WIDTH-1:0] din_f_next;
+assign din_f_next = { 1'b0 , din_f_q[7:1] };
+assign v_f_next   = resetn & din_en;
+initial begin
+	// assume
+	a_reset : assume ( ~resetn );
+end
 
+always @(posedge clk)
+begin
+	din_f_q <= ~resetn ? DATA_WIDTH'bx : 
+					din_en ? din : din_f_next; 
+	v_f_q   <= v_f_next;
+	if ( resetn ) begin
+			sva_shift_reg : assert( ~v_f_q | ( v_f_q & din_f_q[0] == dout )); 
+	end
+end
 `endif
 endmodule
